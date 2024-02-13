45dd045cdb17f4d1871dc8e976555511f020ebd1
rigolinux <rigoberto7567@gmail.com>
2024-02-12 21:54:19 -0600

    add new space for userName

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,8 +1,13 @@
-import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsString, Length } from 'class-validator';
 export class CreateUserDto {
   @IsNotEmpty()
   @IsString()
   name: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @Length(4, 20)
+  username: string;
 
   @IsNotEmpty()
   @IsEmail()
